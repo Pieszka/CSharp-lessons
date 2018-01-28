66cef66605c8f9bc8a67aa240875fb9822f4ba4e
Paulina <paulinapiechaczek@gmail.com>
2018-01-28 17:25:31 +0100

    Classes ond objects
    Sunday, 28/01/18
    Blank list of employers

---
+++ Classes and objects. Company project/Classes and objects. Company project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects.Company_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employers_number;
+            int[] tablica = new int[100];
+           
+            
+
+
+            Console.WriteLine("Welcome to Coderation Company system. As an admin you have ability to enter some data");
+            Console.WriteLine("Pic a number od empleyers working at Coderation. You can enter max. 100.");
+            employers_number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Below, there is a blank list of employers");
+            
+            for (int x = 1; x<= employers_number; x++)
+
+            {
+                Console.WriteLine(x + ". Name, Surname, Status, Salary.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
